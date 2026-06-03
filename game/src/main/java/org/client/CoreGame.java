778b9b0e5e10638dfd09248210199e12a2cca700
Roman Hvozdik <r.hvozdik@ukma.edu.ua>
2026-06-03 15:50:08 +0300

    feat: add test map render

--- game/src/main/java/org/client/CoreGame.java
+++ game/src/main/java/org/client/CoreGame.java
@@ -1,13 +1,44 @@
 package org.client;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
+import lombok.Setter;
+import org.core.data.*;
+import org.core.entity.*;
+import org.core.state.*;
 
 public class CoreGame extends ApplicationAdapter {
+
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera camera;
+    private CameraInputController cameraController;
+    private ShapeRenderer shapeRenderer;
+
     @Override
     public void create() {
-        // Load resources into memory
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        map = new TmxMapLoader().load("maps/tests/test.tmx");
+        float unitScale = 1f;
+        renderer = new OrthogonalTiledMapRenderer(map, unitScale);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1600, 900);
+        camera.update();
+
+        //cameraController = new CameraInputController(camera);
+        //Gdx.input.setInputProcessor(cameraController);
     }
 
     @Override
@@ -27,5 +58,16 @@ public class CoreGame extends ApplicationAdapter {
 
     private void draw() {
         ScreenUtils.clear(Color.BLUE);
+
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
     }
+
+    @Override
+    public void dispose () {
+        map.dispose();
+        renderer.dispose();
+    }
+
 }
