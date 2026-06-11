a9e334b51c9b59f23af5ee82ac437ed354b6cd68
Roman Hvozdik <r.hvozdik@ukma.edu.ua>
2026-06-11 19:55:12 +0300

    refactor: new animation system for enemies

---
+++ game/src/main/java/org/client/EnemyAnimationSet.java
@@ -0,0 +1,29 @@
+package org.client;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnemyAnimationSet{
+    private final Map<AnimationState, Animation<TextureRegion>> animations = new HashMap<>();
+
+    public void addAnimation(AnimationState state, Animation<TextureRegion> anim) {
+        animations.put(state, anim);
+    }
+
+    public TextureRegion getKeyFrame(AnimationState state, float stateTime, boolean loop) {
+        Animation<TextureRegion> anim = animations.get(state);
+        if (anim == null) {
+            anim = animations.get(AnimationState.IDLE);
+        }
+        return anim != null ? anim.getKeyFrame(stateTime, loop) : null;
+    }
+
+    public boolean isAnimationFinished(AnimationState state, float stateTime) {
+        Animation<TextureRegion> anim = animations.get(state);
+        if (anim == null) return true;
+        return anim.isAnimationFinished(stateTime);
+    }
+}
