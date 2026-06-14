84a0a3ab013c45cadd1629511eb4e0cbac0c54a6
Stanislav Koshynskyi <stasony10@gmail.com>
2026-06-14 14:27:39 +0300

    fix: exitdata

--- game/src/main/java/org/core/data/ExitData.java
+++ game/src/main/java/org/core/data/ExitData.java
@@ -7,8 +7,6 @@ public class ExitData {
 
     public ExitData(String exitId, float x, float y, float width, float height) {
         this.exitId = exitId;
-        this.x = x;
-        this.y = y;
         this.width = width;
         this.height = height;
         this.x = x + width / 2;
