55ce504d2806159ed3dea396336079bcc8159921
SecondMelon <y.voloshanovych@ukma.edu.ua>
2026-06-15 13:09:24 +0300

    feat: add graphics to the levels

---
+++ game/src/main/resources/maps/carpet_floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="carpet_floor" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="../tiles/carpets.png" width="192" height="176"/>
+</tileset>
---
+++ game/src/main/resources/maps/level_3_low_obstacles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="low_obstacles_level_3" tilewidth="16" tileheight="16" tilecount="15" columns="5">
+ <image source="../tiles/level_3_low_obstacles.png" width="80" height="48"/>
+</tileset>
